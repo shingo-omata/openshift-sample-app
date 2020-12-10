627aa00da3c31b15304317ad676bf38a7067156b
Shingo Omata <shingift0503@gmail.com>
2020-12-10 12:30:42 +0900

    modify app.py

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask # flaskパッケージの呼び出し
+# インストールしたパッケージのインポート
+from flask import Flask
 
-app = Flask(__name__) # おまじない。Flaskのインスタンスを生成している 
+# appという名前でFlaskのインスタンスを作成
+app = Flask(__name__)
 
-@app.route("/") # ブラウザからアクセスがあった時に実行する処理を書いている。ルートにアクセスするとhelloアクションが呼び出されて、hello Flask!をいう文字列を返す。
-def hello():
-    return "Hello Flask! "
+# どこのアドレスで実行するか指定
+# 今回は http://127.0.0.1:5000/ にアクセスされたらhello_worldを実行するよ
+@app.route('/')
+def hello_world():
 
-if __name__ == "__main__":# これも決まった書き方らしい。
-    app.run(host = '0.0.0.0', port = 5000)
+    message = "Hello World"
+
+    return message
+
+if __name__ == '__main__':
+    # 作成したappを起動
+    # ここでflaskの起動が始まる
+    app.run()
