9061630f59c912b045aaf8dbceb63647f5eebcd3
Shingo Omata <shingift0503@gmail.com>
2020-12-10 12:43:25 +0900

    update

--- app.py
+++ app.py
@@ -1,19 +1,14 @@
-# インストールしたパッケージのインポート
+import os
 from flask import Flask
-
-# appという名前でFlaskのインスタンスを作成
 app = Flask(__name__)
 
-# どこのアドレスで実行するか指定
-# 今回は http://127.0.0.1:5000/ にアクセスされたらhello_worldを実行するよ
-@app.route('/')
-def hello_world():
+@app.route("/")
+def main():
+    return "Welcome!"
 
-    message = "Hello World"
+@app.route('/how are you')
+def hello():
+    return 'I am good, how about you?'
 
-    return message
-
-if __name__ == '__main__':
-    # 作成したappを起動
-    # ここでflaskの起動が始まる
+if __name__ == "__main__":
     app.run()
